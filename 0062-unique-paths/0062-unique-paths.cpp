05c93bd5741c0be6b59b3a589543796d0d467cd9
Bhawarth Garg <79307138+bhawarth-garg@users.noreply.github.com>
2023-06-06 12:52:14 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (52.50%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -19,9 +19,23 @@ public:
     }
     int uniquePaths(int m, int n) {
         
-        vector<vector<int>> dp(m+1, vector<int>(n+1,-1));
+        vector<vector<int>> dp(m, vector<int>(n,0));
+        dp[0][0]=1;
         
-        return solve(dp, 0,0,m,n);
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i>=1)
+                    dp[i][j]+= dp[i-1][j];
+                if(j>=1)
+                    dp[i][j]+= dp[i][j-1];
+                
+            }
+        }
+        // cout<<dp[0][1]<<"  ";
+        
+        return dp[m-1][n-1];
         
     }
 };
