6505c5032c43ae7098368959f26e7ed64c9ad71c
Bhawarth Garg <79307138+bhawarth-garg@users.noreply.github.com>
2023-08-29 08:40:14 +0530

    Time: 282 ms (5.92%) | Memory: 14.8 MB (49.29%) - LeetSync

---
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int bestClosingTime(string c) {
+        int n= c.size();
+        int s=0;
+        vector<int> v(n+1);
+        v[0]=0;
+        for(int i=0;i<n;i++)
+        {
+            if(c[i]=='Y')
+                s+=1;
+            else
+                s-=1;
+            
+            v[i+1]= s;
+        }
+        int maxm= 0, ind=0;
+        for(int i=0;i<n+1;i++)
+        {
+            cout<<v[i]<<" ";
+
+            if(v[i]>maxm)
+            {
+                maxm= v[i];
+                ind=i;
+            }
+
+
+        }
+        return ind;
+
+
+
+        
+    }
+};
